cffdf3a4b2db9078140e40684fe040c493743171
LHoogenboom <hoogenboom.l.p@gmail.com>
2020-12-30 17:44:44 +0100

    solves question 1

---
+++ ctcont.m
@@ -0,0 +1,27 @@
+clear; clc;
+
+%% Plant
+
+G = tf((1),[0.2 1.4 2.8 4]);
+Gzpk = zpk(G);
+
+%% Controller
+Kp = 15.7/2;
+Ki = 11.5;
+Kd = 4;
+
+C = tf([Kd Kp Ki], [1 0]);
+
+%% System
+L = C*G;
+H = zpk(L/(1+L));
+
+%% Simulation
+t = 0:0.01:99.99;
+u = ones(length(t),1);
+figure(1);
+lsim(H,u,t);
+grid on;
+grid minor;
+set(gca,'xtick',[0:5:100]);
+set(gca,'ytick',[0:.05:2]);
